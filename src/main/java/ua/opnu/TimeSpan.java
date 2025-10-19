198662e8245b33451469f901a82921fccc16cef8
Molyavko Evgeniya <9152197@stud.op.edu.ua>
2025-10-19 22:47:29 +0300

    changed TimeSpan

--- src/main/java/ua/opnu/TimeSpan.java
+++ src/main/java/ua/opnu/TimeSpan.java
@@ -27,22 +27,17 @@ public class TimeSpan {
         return minutes;
     }
 
-    void add(int hours, int minutes) {
-        if (hours < 0) {
-            this.hours = getHours();
-        } else if(minutes < 0){
-            this.minutes = getMinutes();
-        } else if (minutes >= 60) {
-            int hoursAdded = minutes / 60;
-            this.hours = hours + hoursAdded + getHours();
-            this.minutes = (minutes - (hoursAdded * 60)) + getMinutes();
-        } else if ((minutes + getMinutes()) >= 60) {
-            int hoursAdded = (minutes + getMinutes()) / 60;
-            this.hours = hours + hoursAdded + getHours();
-            this.minutes = (minutes - (hoursAdded * 60)) + getMinutes();
-        } else {
-            this.hours = hours + getHours();
-            this.minutes = minutes + getMinutes();
+    public void add(int hours, int minutes) {
+        if(hours > 0) {
+            this.hours += hours;
+        }
+        if(minutes > 0) {
+            this.minutes += minutes;
+        }
+        if(this.minutes >= 60) {
+            int hoursAdded = this.minutes / 60;
+            this.hours += hoursAdded;
+            this.minutes = (this.minutes - (hoursAdded * 60));
         }
 
     }
@@ -61,21 +56,15 @@ public class TimeSpan {
         return sumMinutes;
     }
 
-    void subtract(TimeSpan span) {
+    public void subtract(TimeSpan span) {
         int timeFirst = (getHours()*60) + getMinutes();
         int timeSecond = (span.getHours()*60) + span.getMinutes();
         if(timeFirst>=timeSecond){
             int result = timeFirst - timeSecond;
             hours = result/60;
-            minutes = result%60;
-        } else {
-            hours = getHours();
-            minutes = getMinutes();
+            minutes = result - (hours*60);
         }
-
-
     }
-
     void scale(int factor) {
         if(factor > 0){
             int minSum = factor*((getHours()*60) + getMinutes());
